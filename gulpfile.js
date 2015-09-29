61681ae738e06c5300063aa2f7f23e754ddeabbc
branespace <branespace@users.noreply.github.com>
2015-09-29 09:14:23 -0700

    Add angular support

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var jshint = require('gulp-jshint');
 var gulpMocha = require('gulp-mocha');
+var watch = require('gulp-watch');
 
 gulp.task('webpack:dev', function() {
   return gulp.src('./app/js/client.js')
@@ -25,9 +26,14 @@ gulp.task('jshint', function() {
 });
 
 gulp.task('test', function() {
-  return gulp.src('test/**/*test.js')
+  return gulp.src('test/**/*tests.js')
     .pipe(gulpMocha({reporter: 'nyan'}));
 });
 
+gulp.task('watch', function() {
+  return gulp.watch(['app/**/*'], ['build:dev']);
+});
+
 gulp.task('build:dev', ['staticfiles:dev', 'webpack:dev']);
+gulp.task('tests', ['jshint', 'test']);
 gulp.task('default', ['build:dev']);
